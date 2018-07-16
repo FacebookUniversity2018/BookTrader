0240cc0981254bc201996a67f22e5c9519c249b8
Connor Clancy <connorclancy@fb.com>
2018-07-16 10:21:44 -0700

    xcode proj to work on

---
+++ BarcodeAPI/BarcodeAPI/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BarcodeAPI
+//
+//  Created by Connor Clancy on 7/16/18.
+//  Copyright © 2018 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ BookTrader/BookTrader/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  BookTrader
+//
+//  Created by Connor Clancy on 7/16/18.
+//  Copyright © 2018 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
