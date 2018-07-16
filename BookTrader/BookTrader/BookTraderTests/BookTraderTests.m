ae529f3c61602f318d56a54c8bc7cddb35d72f0d
Claudia Haddad <cmhaddad@fb.com>
2018-07-16 10:29:57 -0700

    first

---
+++ BookTrader/BookTrader/BookTraderTests/BookTraderTests.m
@@ -0,0 +1,39 @@
+//
+//  BookTraderTests.m
+//  BookTraderTests
+//
+//  Created by Claudia Haddad on 7/16/18.
+//  Copyright © 2018 Claudia Haddad. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface BookTraderTests : XCTestCase
+
+@end
+
+@implementation BookTraderTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
