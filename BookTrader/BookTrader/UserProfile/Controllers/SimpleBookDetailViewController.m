37447e63a3675767b4fcc11ae4366f19719734c3
Claudia Haddad <claudiahaddad@bellsouth.net>
2018-07-17 11:02:28 -0700

    added all 3 book detail view controllers

---
+++ BookTrader/BookTrader/UserProfile/Controllers/SimpleBookDetailViewController.m
@@ -0,0 +1,37 @@
+//
+//  SimpleBookDetailViewController.m
+//  BookTrader
+//
+//  Created by Claudia Haddad on 7/17/18.
+//  Copyright © 2018 codepath. All rights reserved.
+//
+
+#import "SimpleBookDetailViewController.h"
+
+@interface SimpleBookDetailViewController ()
+
+@end
+
+@implementation SimpleBookDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
