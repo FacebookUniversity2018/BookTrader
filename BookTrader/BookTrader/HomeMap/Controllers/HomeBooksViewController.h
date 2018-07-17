438c4bca83ece3e731428cd6abd58caa22636da5
Connor Clancy <connorclancy@fb.com>
2018-07-17 15:49:44 -0700

    my books home first draft done

---
+++ BookTrader/BookTrader/HomeMap/Controllers/HomeBooksViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeBooksViewController.h
+//  BookTrader
+//
+//  Created by Connor Clancy on 7/17/18.
+//  Copyright © 2018 codepath. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeBooksViewController : UIViewController
+
+@end
---
+++ BookTrader/BookTrader/HomeMap/Views/BookCell.m
@@ -0,0 +1,13 @@
+//
+//  BookCell.m
+//  BookTrader
+//
+//  Created by Connor Clancy on 7/17/18.
+//  Copyright © 2018 codepath. All rights reserved.
+//
+
+#import "BookCell.h"
+
+@implementation BookCell
+
+@end
