601bed4ab3a7ab3d2e36378d3fe9caa289aa940c
Claudia Haddad <claudiahaddad@bellsouth.net>
2018-07-18 10:23:06 -0700

    books api

---
+++ APIs/BookAPI/BookAPI/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BookAPI
+//
+//  Created by Claudia Haddad on 7/17/18.
+//  Copyright © 2018 Claudia Haddad. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ APIs/BookAPI/BookAPI/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  BookAPI
+//
+//  Created by Claudia Haddad on 7/17/18.
+//  Copyright © 2018 Claudia Haddad. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
