139c93b83bf4058d0236c8aab01eb0754c2182f6
Connor Clancy <connorclancy@fb.com>
2018-07-18 11:08:53 -0700

    barcode scanner working in app

--- BookTrader/BookTrader/AddBook/Controllers/BarcodeAddViewController.m
+++ BookTrader/BookTrader/AddBook/Controllers/BarcodeAddViewController.m
@@ -7,8 +7,15 @@
 //
 
 #import "BarcodeAddViewController.h"
+#import <AVFoundation/AVFoundation.h>
 
-@interface BarcodeAddViewController ()
+
+@interface BarcodeAddViewController () <AVCaptureMetadataOutputObjectsDelegate>
+@property (strong, nonatomic) IBOutlet UIView *previewView;
+@property (strong, nonatomic) AVCaptureSession *captureSession;
+@property (strong, nonatomic) AVCaptureVideoPreviewLayer *videoPreviewLayer;
+
+-(void) stopReading;
 
 @end
 
@@ -19,19 +26,52 @@
     // Do any additional setup after loading the view.
 }
 
+- (void)viewDidAppear:(BOOL)animated {
+    self.captureSession = nil;
+    NSError *error;
+    AVCaptureDevice *captureDevice = [AVCaptureDevice defaultDeviceWithMediaType:AVMediaTypeVideo];
+    AVCaptureDeviceInput *input = [AVCaptureDeviceInput deviceInputWithDevice:captureDevice error:&error];
+    self.captureSession = [AVCaptureSession new];
+    [self.captureSession addInput:input];
+    AVCaptureMetadataOutput *captureMetadataOutput = [AVCaptureMetadataOutput new];
+    [self.captureSession addOutput:captureMetadataOutput];
+    dispatch_queue_t dispatchQueue;
+    dispatchQueue = dispatch_queue_create("myQueue", NULL);
+    [captureMetadataOutput setMetadataObjectsDelegate:self queue:dispatchQueue];
+    [captureMetadataOutput setMetadataObjectTypes:[NSArray arrayWithObject:AVMetadataObjectTypeEAN13Code]];
+    self.videoPreviewLayer = [[AVCaptureVideoPreviewLayer alloc] initWithSession:self.captureSession];
+    [self.videoPreviewLayer setVideoGravity:AVLayerVideoGravityResizeAspectFill];
+    [self.videoPreviewLayer setFrame:self.previewView.layer.bounds];
+    [self.previewView.layer addSublayer:self.videoPreviewLayer];
+    [self.captureSession startRunning];
+}
+
+-(void)captureOutput:(AVCaptureOutput *)output didOutputMetadataObjects:(NSArray<__kindof AVMetadataObject *> *)metadataObjects fromConnection:(AVCaptureConnection *)connection {
+    if (metadataObjects != nil && [metadataObjects count] > 0) {
+        AVMetadataMachineReadableCodeObject *metadataObj = [metadataObjects objectAtIndex:0];
+        if ([[metadataObj type] isEqualToString:AVMetadataObjectTypeEAN13Code]) {
+            NSLog([metadataObj stringValue]);
+            [self performSelectorOnMainThread:@selector(stopReading) withObject:[metadataObj stringValue] waitUntilDone:NO];
+        }
+    }
+}
+
+-(void)stopReading {
+    [self.captureSession stopRunning];
+    self.captureSession = nil;
+    [self.videoPreviewLayer removeFromSuperlayer];
+    [self performSegueWithIdentifier:@"barcodeToDetailsSegue" sender:nil];
+}
+
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
 }
-*/
 
 @end
