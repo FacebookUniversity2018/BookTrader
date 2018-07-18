64900e23518b4405cdc81e69486fcfa10329f134
Chaliana Rolon <chaliana@fb.com>
2018-07-18 11:16:52 -0700

    Successfully retrieved and displayed the user image, created an UIImage to hold raw image data for future PFFIle

--- APIs/FBLoginAPI/loginFBtest/loginFBtest/ProfileViewController.m
+++ APIs/FBLoginAPI/loginFBtest/loginFBtest/ProfileViewController.m
@@ -9,9 +9,12 @@
 #import "ProfileViewController.h"
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <FBSDKLoginKit/FBSDKLoginKit.h>
+#import "UIImageView+AFNetworking.h"
 
 @interface ProfileViewController ()
-
+    
+    @property (weak, nonatomic) IBOutlet UIImageView *rawImage;
+    
 @end
 
 @implementation ProfileViewController
@@ -20,6 +23,15 @@
     [super viewDidLoad];
     NSLog(@"Succesfully loaded Profile VC");
     self.username.text = self.name;
+    [self.profileImage setImageWithURL:self.pictureURL];
+    
+    // Code to retrieve the UIImage raw data
+    NSData *imageData = [NSData dataWithContentsOfURL:self.pictureURL];
+    UIImage *image = [UIImage imageWithData:imageData];
+    if(image!=nil) {
+        NSLog(@"Image retrieved");
+        self.rawImage.image = image;
+    }
 }
 
 - (void)didReceiveMemoryWarning {
