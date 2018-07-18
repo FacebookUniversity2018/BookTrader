7e9539fd8f84408294e0cfd9f105ceb74b61874b
Chaliana Rolon <chaliana@fb.com>
2018-07-18 14:44:03 -0700

    Trying to convert UIImage to PFFIle

--- APIs/FBLoginAPI/loginFBtest/loginFBtest/ProfileViewController.m
+++ APIs/FBLoginAPI/loginFBtest/loginFBtest/ProfileViewController.m
@@ -10,10 +10,12 @@
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <FBSDKLoginKit/FBSDKLoginKit.h>
 #import "UIImageView+AFNetworking.h"
+#import "Parse.h"
+#import "ParseUI.h"
 
 @interface ProfileViewController ()
     
-    @property (weak, nonatomic) IBOutlet UIImageView *rawImage;
+    @property (weak, nonatomic) IBOutlet PFImageView *rawImage;
     
 @end
 
@@ -28,15 +30,65 @@
     // Code to retrieve the UIImage raw data
     NSData *imageData = [NSData dataWithContentsOfURL:self.pictureURL];
     UIImage *image = [UIImage imageWithData:imageData];
-    if(image!=nil) {
+    
+    // For Parse size purposes
+    [self resizeImage:image withSize:CGSizeMake(50, 50)];
+    
+    if(image!=nil)  {
         NSLog(@"Image retrieved");
         self.rawImage.image = image;
+        // Get PFFile
+        PFFile *imageFile = [ProfileViewController getPFFileFromImage:self.rawImage.image];
+        if(imageFile!=nil) {
+            NSLog(@"Created pffile for image");
+        }
     }
+}
+- (IBAction)onBack:(id)sender {
+    [self dismissViewControllerAnimated:YES completion:nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+ 
+/**
+ Method to convert UIImage to PFFile:
+ 
+ - parameter image: Image that the user wants to upload to parse
+ - returns: PFFile for the the data in the image
+ */
++ (PFFile *)getPFFileFromImage: (UIImage * _Nullable)image {
+    
+    // check if image is not nil
+    if (!image) {
+        return nil;
+    }
+    
+    NSData *imageData = UIImagePNGRepresentation(image);
+    // get image data and check if that is not nil
+    if (!imageData) {
+        return nil;
+    }
+    
+    return [PFFile fileWithName:@"image.png" data:imageData];
+}
+  
+    
+    
+- (UIImage *)resizeImage:(UIImage *)image withSize:(CGSize)size {
+    UIImageView *resizeImageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, size.width, size.height)];
+    
+    resizeImageView.contentMode = UIViewContentModeScaleAspectFill;
+    resizeImageView.image = image;
+    
+    UIGraphicsBeginImageContext(size);
+    [resizeImageView.layer renderInContext:UIGraphicsGetCurrentContext()];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    return newImage;
 }
 
 /*
