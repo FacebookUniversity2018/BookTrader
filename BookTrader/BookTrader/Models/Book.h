8f36bce59f2a6dc8e1772fc34ca34397b0dd0cd0
Connor Clancy <connorclancy@fb.com>
2018-07-17 17:01:48 -0700

    parse pods added

--- BookTrader/BookTrader/Models/Book.h
+++ BookTrader/BookTrader/Models/Book.h
@@ -7,7 +7,12 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "Parse/Parse.h"
 
 @interface Book : NSObject
+@property (strong, nonatomic) NSString *title;
+@property (strong, nonatomic) NSString *author;
+@property (strong, nonatomic) NSString *overview;
 
++(void)addBookToDatabase:(NSString *)title withAuthor:(NSString *)author withOverview:(NSString *overview) withCompletion:(PFBooleanResultBlock _Nullable)completion;
 @end
