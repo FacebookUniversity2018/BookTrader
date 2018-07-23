8141f1d711f63f42730cf14685700277392617a5
Connor Clancy <connorwilliamclancy@gmail.com>
2018-07-23 11:45:18 -0700

    pods fix

--- Demos/FBLoginAPI/facebookPFUserLogin/Pods/Target Support Files/Pods-facebookPFUserLogin/Pods-facebookPFUserLogin-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_facebookPFUserLogin : NSObject
-@end
-@implementation PodsDummy_Pods_facebookPFUserLogin
-@end
--- Demos/FBLoginAPI/facebookPFUserLogin/Pods/Target Support Files/Pods-facebookPFUserLoginTests/Pods-facebookPFUserLoginTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_facebookPFUserLoginTests : NSObject
-@end
-@implementation PodsDummy_Pods_facebookPFUserLoginTests
-@end
